5ea70fb5b6d110a11b90f72c2213900fbdba847a
Hebok Pal <hebokpal1991@gmail.com>
2015-05-26 16:57:08 +0200

    Regenerated App class after updating changing icons to be stored as data instead of URL.

--- App.h
+++ App.h
@@ -2,7 +2,7 @@
 //  App.h
 //  WebServiceTut
 //
-//  Created by Hebok Pal on 5/19/15.
+//  Created by Hebok Pal on 5/26/15.
 //  Copyright (c) 2015 Bitfall. All rights reserved.
 //
 
@@ -21,15 +21,15 @@
 @property (nonatomic, retain) NSNumber * currentPrice;
 @property (nonatomic, retain) NSDate * currentPriceDate;
 @property (nonatomic, retain) NSString * downloadURL;
-@property (nonatomic, retain) NSString * icon;
-@property (nonatomic, retain) NSString * iconLarge;
+@property (nonatomic, retain) NSData * icon;
+@property (nonatomic, retain) NSData * iconLarge;
 @property (nonatomic, retain) NSNumber * iD;
+@property (nonatomic, retain) NSNumber * previousPrice;
 @property (nonatomic, retain) NSNumber * primaryCategoryID;
 @property (nonatomic, retain) NSString * primaryCategoryTitle;
 @property (nonatomic, retain) NSString * title;
 @property (nonatomic, retain) NSNumber * userRatingCountForAllVersion;
 @property (nonatomic, retain) NSNumber * userRatingCountForCurrentVersion;
-@property (nonatomic, retain) NSNumber * previousPrice;
 @property (nonatomic, retain) AppDeal *appDeal;
 @property (nonatomic, retain) NSSet *screenshots;
 @end
--- App.m
+++ App.m
@@ -2,7 +2,7 @@
 //  App.m
 //  WebServiceTut
 //
-//  Created by Hebok Pal on 5/19/15.
+//  Created by Hebok Pal on 5/26/15.
 //  Copyright (c) 2015 Bitfall. All rights reserved.
 //
 
@@ -24,12 +24,12 @@
 @dynamic icon;
 @dynamic iconLarge;
 @dynamic iD;
+@dynamic previousPrice;
 @dynamic primaryCategoryID;
 @dynamic primaryCategoryTitle;
 @dynamic title;
 @dynamic userRatingCountForAllVersion;
 @dynamic userRatingCountForCurrentVersion;
-@dynamic previousPrice;
 @dynamic appDeal;
 @dynamic screenshots;
 
