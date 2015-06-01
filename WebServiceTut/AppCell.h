db2d3fec370873d7947876b473037e5215d449c9
Hebok Pal <hebokpal1991@gmail.com>
2015-06-01 14:30:35 +0200

    Created class for UITableView cells representing apps.

---
+++ WebServiceTut/AppCell.h
@@ -0,0 +1,16 @@
+//
+//  AppCell.h
+//  WebServiceTut
+//
+//  Created by Hebok Pal on 5/27/15.
+//  Copyright (c) 2015 Bitfall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppCell : UITableViewCell
+
+@property (nonatomic, strong) IBOutlet UIImageView *appIcon;
+@property (nonatomic, strong) IBOutlet UILabel *titleLabel;
+
+@end
---
+++ WebServiceTut/AppCell.m
@@ -0,0 +1,17 @@
+//
+//  AppCell.m
+//  WebServiceTut
+//
+//  Created by Hebok Pal on 5/27/15.
+//  Copyright (c) 2015 Bitfall. All rights reserved.
+//
+
+#import "AppCell.h"
+
+@implementation AppCell
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+}
+
+@end
