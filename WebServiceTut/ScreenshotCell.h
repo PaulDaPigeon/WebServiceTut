efddb3c490e3ab023169a29819d6a7cc0f1ff956
Hebok Pal <hebokpal1991@gmail.com>
2015-06-01 14:31:51 +0200

    Created class for the cells of the UICollectionView presenting the screenshots in the detail view.

---
+++ WebServiceTut/ScreenshotCell.h
@@ -0,0 +1,14 @@
+//
+//  ScreenshotCell.h
+//  WebServiceTut
+//
+//  Created by Hebok Pal on 6/1/15.
+//  Copyright (c) 2015 Bitfall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScreenshotCell : UICollectionViewCell
+@property (nonatomic, strong) IBOutlet UIImageView *screenshot;
+
+@end
---
+++ WebServiceTut/ScreenshotCell.m
@@ -0,0 +1,13 @@
+//
+//  ScreenshotCell.m
+//  WebServiceTut
+//
+//  Created by Hebok Pal on 6/1/15.
+//  Copyright (c) 2015 Bitfall. All rights reserved.
+//
+
+#import "ScreenshotCell.h"
+
+@implementation ScreenshotCell
+
+@end
