a144b3a8efae7dc99fcd9be2061adc6ccee35147
Hebok Pal <hebokpal1991@gmail.com>
2015-06-02 14:47:57 +0200

    Created class for view screenshots.

---
+++ WebServiceTut/ScreenshotCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScreenshotCollectionViewController.h
+//  WebServiceTut
+//
+//  Created by Hebok Pal on 6/1/15.
+//  Copyright (c) 2015 Bitfall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScreenshotCollectionViewController : UICollectionViewController
+
+@end
